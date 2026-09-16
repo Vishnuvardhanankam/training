c34e140b0f3b81d625639e81d4cbaaad202f5b8c
Vishnu <ankamvishnuvardhan8@gmail.com>
2026-09-16 22:22:18 +0530

    primecheck

--- primecheck.java
+++ primecheck.java
@@ -4,8 +4,8 @@ public static Boolean isPrime(int n){
     if(n<2){
         return false;
     }
-    for(int i=2;i<Math.sqrt(n);i++){
-        if(n%2==0){
+    for(int i=2;i<=Math.sqrt(n);i++){
+        if(n%i==0){
             return false;
         }
     }
