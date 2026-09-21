125732da6641c1f066b956e442b9fd0c7d183afe
Vishnu <ankamvishnuvardhan8@gmail.com>
2026-09-21 14:08:14 +0530

    encrpytion

---
+++ encrp.java
@@ -0,0 +1,22 @@
+public class encrp {
+    public static void main(String[] args) {
+        String input="Zomato";
+        int k=92;
+        k=k % 26;
+        StringBuilder result=new StringBuilder();
+        for(char c:input.toCharArray()){
+            if(Character.isLetter(c)){
+                if(Character.isUpperCase(c)){
+                    char encrp=(char)(((c-65+k)%26)+65);
+                    result.append(encrp);
+                }else{
+                    char encrp=(char)(((c-97+k)%26)+97);    
+                    result.append(encrp);
+                }
+            }
+        }
+        System.out.println(result);
+
+    }
+
+}
