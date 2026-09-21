985c157dccffa55daf3d96dc5927fca9d783646c
Vishnu <ankamvishnuvardhan8@gmail.com>
2026-09-21 11:36:34 +0530

    move

---
+++ movehash.java
@@ -0,0 +1,25 @@
+/**
+ * movehash
+ */
+public class movehash {
+    public static void main(String[] args) {
+        String name="";
+        String hash="";
+        String s1="V##i#####s###H";
+        // String s2=s1.toLowerCase();
+        char[] A1=s1.toCharArray();
+        for(char ch:A1){
+            if(Character.isAlphabetic(ch)){
+                name+=ch;
+            }
+            else{
+                hash+=ch;
+            }
+        }
+        System.out.println(name);
+        System.out.println(name+hash);
+        System.out.println(hash+name);
+    }
+
+    
+}
