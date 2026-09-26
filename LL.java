1e3315ee3ac69a071a797bc42c798f1539f6f244
Vishnu <ankamvishnuvardhan8@gmail.com>
2026-09-26 10:50:36 +0530

    LinkedList

---
+++ LL.java
@@ -0,0 +1,49 @@
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+class Linkedlist {
+    Node head;
+    void insertAtBeginning(int data){
+        Node newNode=new Node(data);
+        newNode.next=head;
+        head=newNode;
+    }
+    void insertAtEnd(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        Node temp=head;
+        while (temp.next!=null) {
+            temp=temp.next;
+        }
+        temp.next=newNode;
+    }
+    void display(){
+        Node temp=head;
+        while (temp!=null) {
+            System.out.print(temp.data +"->");
+            temp=temp.next;   
+        }
+        System.out.println("null");
+    }
+}
+public class LL {
+    public static void main(String[] args) {
+        Linkedlist list = new Linkedlist();
+        list.insertAtEnd(30);
+        list.insertAtEnd(40);
+        list.insertAtEnd(50);
+        System.out.println("Original list");
+        list.display();
+        list.insertAtBeginning(10);
+        list.insertAtBeginning(70);
+        list.display();
+    }
+}
